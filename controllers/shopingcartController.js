255263e5fce84178212274ca87dd1672088e2475
Priyanka Vansadia <co858725@mylambton.ca>
2023-08-11 23:46:41 -0400

    shooping cart bug solved

--- controllers/shopingcartController.js
+++ controllers/shopingcartController.js
@@ -6,18 +6,7 @@ var session;
 
 exports.getShopingCartData = async function (req, res) {
   session = req.session;
-  const cartdetails = getAllCartData()
-  const productData=getAllProductsFromCart(cartdetails)
-  res.render('shoping-cart', { title_page:"cart",products: productData, user_id: session.user_id, firstname: session.firstname, lastname: session.lastname, email: session.email })
-}
-
-var getAllCartData=exports.getAllCartData=async function()
-{
-  return await cart.find({is_activated:true })
-}
-
-var getAllProductsFromCart=exports.getAllProductsFromCart=async function(cartdetails)
-{
+  const cartdetails = await cart.find({is_activated:true })
   var productdetails = [];
   for (let i = 0; i < cartdetails.length; i++) {
     var productinfo = await product.findOne({ _id: cartdetails[i].product_id })
@@ -26,9 +15,9 @@ var getAllProductsFromCart=exports.getAllProductsFromCart=async function(cartdet
       productdetails.push(productinfo)
     }
   }
-  return productdetails;
+  console.info("product data: "+productdetails)
+  res.render('shoping-cart', { title_page:"cart",products: productdetails, user_id: session.user_id, firstname: session.firstname, lastname: session.lastname, email: session.email })
 }
-
 
 exports.addToCart = async function (req, res) {
   session = req.session;
@@ -49,21 +38,34 @@ exports.addToCart = async function (req, res) {
     })
     newCart.save();
   }
-  const cartdetails = getAllCartData()
-  const productData=getAllProductsFromCart(cartdetails)
-  res.render('shoping-cart', {title_page:"cart", products: productData, user_id: session.user_id, firstname: session.firstname, lastname: session.lastname, email: session.email })
+  const cartdetails = await cart.find({is_activated:true })
+  var productdetails = [];
+  for (let i = 0; i < cartdetails.length; i++) {
+    var productinfo = await product.findOne({ _id: cartdetails[i].product_id })
+    if (productinfo != null) {
+      productinfo.quantity = cartdetails[i].quantiy
+      productdetails.push(productinfo)
+    }
+  }
+  res.render('shoping-cart', {title_page:"cart", products: productdetails, user_id: session.user_id, firstname: session.firstname, lastname: session.lastname, email: session.email })
 }
 
 exports.deleteProduct = async function (req, res) {
   session = req.session;
   const filter = { product_id: req.params.id, user_id: session.user_id, is_activated:true  };
-  console.info("param info: "+filter)
   const deletedproduct = await cart.findOneAndDelete(filter).then()
   {
     console.info("deleted successfully")
   }
-  const cartdetails = getAllCartData()
-  const productData=getAllProductsFromCart(cartdetails)
-  res.render('shoping-cart', {title_page:"cart", products: productData, user_id: session.user_id, firstname: session.firstname, lastname: session.lastname, email: session.email })
+  const cartdetails = await cart.find({is_activated:true })
+  var productdetails = [];
+  for (let i = 0; i < cartdetails.length; i++) {
+    var productinfo = await product.findOne({ _id: cartdetails[i].product_id })
+    if (productinfo != null) {
+      productinfo.quantity = cartdetails[i].quantiy
+      productdetails.push(productinfo)
+    }
+  }
+  res.render('shoping-cart', {title_page:"cart", products: productdetails, user_id: session.user_id, firstname: session.firstname, lastname: session.lastname, email: session.email })
 }
 
