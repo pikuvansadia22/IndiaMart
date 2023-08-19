1ddc8a8c07d6ba1616a3773c37d94c0a74087883
Ash Gajjar <c0857523@mylambton.ca>
2023-08-18 22:03:26 -0400

    category crud operations are completed

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -8,18 +8,62 @@ var fs = require('fs');
 var session;
 
 exports.getAdminData = async function (req, res) {
-
   const categories = await category.find()
   session = req.session;
   res.render('adminhome', { title_page: "Category", categories: categories, user_id: session.user_id, firstname: session.firstname, lastname: session.lastname, email: session.email })
 }
 
 exports.getLoginData = async function (req, res) {
+  session = req.session;
+  res.render('category', { title_page: "Category",categories: null, user_id: session.user_id, firstname: session.firstname, lastname: session.lastname, email: session.email })
+}
 
-  const categories = await category.find()
+exports.getCategoryById = async function (req, res) {
+  const categories = await category.findOne({_id:req.params.id})
   session = req.session;
   res.render('category', { title_page: "Category", categories: categories, user_id: session.user_id, firstname: session.firstname, lastname: session.lastname, email: session.email })
 }
+
+exports.getViewCategory = async function (req, res) {
+
+  const categories = await category.find()
+  session = req.session;
+  res.render('viewcategories', { title_page: "Category", categories: categories, user_id: session.user_id, firstname: session.firstname, lastname: session.lastname, email: session.email })
+}
+
+exports.updateCateogry = async function (req, res) {
+  var update;
+  if (!req.file) {
+    console.log("file not uploaded")
+    update={name:req.body.name}
+  }
+  else
+  {
+    update={name:req.body.name,image: {data: fs.readFileSync(req.file.path),contentType: 'image/png'}}
+  }
+  const filter={_id:req.body.id}
+  const categories = await category.findOneAndUpdate(filter,update).then(cat=>
+    {
+    console.info("success")
+    }
+  ).catch(err=>
+    {
+      res.send(err)
+    }
+  )
+  session = req.session;
+  res.redirect('viewcategories')
+}
+
+exports.deleteCategory = async function (req, res) {
+  const categori = await category.findOneAndDelete({_id:req.params.id}).then(
+    console.log("deleted successfully")
+  )
+  const categories = await category.find()
+  session = req.session;
+  res.render('viewcategories', { title_page: "Category", categories: categories, user_id: session.user_id, firstname: session.firstname, lastname: session.lastname, email: session.email })
+  }
+
 
 exports.saveCateogry = function (req, res) {
 
@@ -36,7 +80,7 @@ exports.saveCateogry = function (req, res) {
 
   newCategory.save().then((createdCategory => {
     //res.status(201).json(createdUser)
-    res.redirect('category')
+    res.redirect('viewcategories')
   })).catch((err => {
     res.status(500).json({
       error: err,
