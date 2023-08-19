df0f63fbee8fe294e0bce596ac86ff6b0d6bdbcd
AshGajjar <c0857523@mylambton.ca>
2023-08-18 21:16:18 -0400

    orderDetails in profile completed

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,25 +1,31 @@
 const mongoose = require('mongoose')
 const user = require('../models/user');
 const address = require('../models/address');
+const product = require('../models/product');
+const order = require('../models/order');
+const cart = require('../models/cart');
 
 var session;
 
 exports.getLoginData = async function (req, res) {
     session = req.session;
     const user_=await user.findById({_id:session.user_id})
-   console.info("user address is : "+user.address_id)
-   if(user.address_id===undefined && user.address_id==null)
+   console.info("user address is : " )
+   if(user_.address_id===undefined || user_.address_id==null)
    {
-    res.render('profile', {title_page:"profile",address:null,user:user_,user_id:session.user_id,
+    res.render('profile', {title_page:"profile",orders:null,address:null,user:user_,user_id:session.user_id,
     firstname:session.firstname,
     lastname:session.lastname,
     email:session.email
 })
    }
    else{
+    const orders = await order.find({user_id:user_._id})
+    console.log("orders",orders)
+  
     const address_=await address.findById({_id:user_.address_id})
     //console.info("user login details", req.session.user )
-    res.render('profile', {title_page:"profile",address:address_,user:user_,user_id:session.user_id,
+    res.render('profile', {title_page:"profile",orders:orders,address:address_,user:user_,user_id:session.user_id,
             firstname:session.firstname,
             lastname:session.lastname,
             email:session.email
